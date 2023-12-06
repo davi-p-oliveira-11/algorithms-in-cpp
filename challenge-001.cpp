6b57b85b71f863faf9b8b74f451f897cc6e09164
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-06 15:52:06 -0300

    Create challenge-001.cpp

---
+++ challenge-001.cpp
@@ -0,0 +1,17 @@
+/*
+ 1) Write a program that displays the message 'Hello, World!' on the screen
+*/
+
+#include <iostream>
+
+int main () {
+  
+  // Show the Hallo Mars message
+  std::cout << "Hallo, Mars"<< std::endl;
+
+  // This ensures that the user sees the message
+  std::cout << "Press Enter to exit...";
+  std::cin.get();  
+
+  return 0;
+}
