4048c018ff4b45957d1bfdfaa22fa2e595456f27
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-07 02:14:07 -0300

    Update challenge-006.cpp

--- challenge-006.cpp
+++ challenge-006.cpp
@@ -7,3 +7,29 @@ The successor of 9 is 10
 */
 
 /* */
+
+#include <iostream>
+
+int main () {
+
+  // Variables
+  int number, predecessor, successor;
+
+  // Prompt the user to enter a number
+  std::cout << "Enter a number: ";
+  std::cin >> number;
+
+  // Calculate the predecessor and successor
+  predecessor = number - 1;
+  successor = number + 1;
+
+  // Display the message
+  std::cout << "The predecessor of " << number << " is " << predecessor << std::endl;
+  std::cout << "The successor of " << number << " is " << successor << std::endl;
+
+  // This ensures that the user sees the message
+  std::cout << "Press Enter to exit...";
+  std::cin.get(); 
+
+  return 0;
+}
