d71a4391518afbe8a501e440a8858482e761616b
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-07 01:03:44 -0300

    Update challenge-003.cpp

--- challenge-003.cpp
+++ challenge-003.cpp
@@ -7,3 +7,34 @@ Employee Jane Doe has a salary of R$1850.45 in June.
 */
 
 /* */
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+int main () {
+
+  // Variables
+  std::string userName;
+  float salary;
+
+  // Prompt the user to enter their name and then storing the full line in the Username Variable
+  std::cout << "What is the employee name ? ";
+  std::getline(std::cin, userName);
+
+  // Prompt the user to enter the value of the salary
+  std::cout << "What is the value of the employee salary ? ";
+  std::cin >> salary;
+
+  // Set the precision to control the number of decimal numbers
+  std::cout << std::fixed << std::setprecision(2);
+
+  // Display the message
+  std::cout << "The employee " << userName << " has a salary of " << salary << " USD" << std::endl;
+
+  // This ensures that the user sees the message
+  std::cout << "Press Enter to exit...";
+  std::cin.get();  
+
+  return 0;
+}
