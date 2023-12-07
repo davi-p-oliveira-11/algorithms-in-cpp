c1b91f314e842f0163860621c302a91e53354828
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-07 01:32:51 -0300

    Update challenge-004.cpp

--- challenge-004.cpp
+++ challenge-004.cpp
@@ -5,3 +5,34 @@ Enter a value: 8
 Enter another value: 5
 The sum of 8 and 5 is equal to 13.
 */
+
+/* */
+
+
+#include <iostream>
+
+int main () {
+  
+  // Variables
+  int num1, num2, sum;
+
+  // Prompt the user to enter the first number
+  std::cout << "Enter one number: ";
+  std::cin >> num1;
+
+  // Prompt the user to enter the second number
+  std::cout << "Enter another number: ";
+  std::cin >> num2;
+
+  // Sum the two numbers
+  sum = num1 + num2;
+
+  // Display the message
+  std::cout << "The sum of " << num1 << " and " << num2 << " is equal to: " << sum << std::endl;
+
+  // This ensures that the user sees the message
+  std::cout << "Press Enter to exit...";
+  std::cin.get();  
+
+  return 0;
+}
