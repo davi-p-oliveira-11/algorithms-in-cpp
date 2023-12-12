cf6e5553ca8e3fa657c94d15ec8c18bcdda2e097
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-12 13:29:39 -0300

    Update challenge-011.cpp

--- challenge-011.cpp
+++ challenge-011.cpp
@@ -3,3 +3,39 @@
  from a quadratic equation and displays the value of Delta.
 */
 
+#include <iostream>
+#include <iomanip>
+
+int main () {
+
+  // Variables
+  float valueA, valueB, valueC, delta;
+
+  // Prompt the user to enter the value of A
+  std::cout << "Enter the value of A ";
+  std::cin >> valueA;
+
+  // Prompt the user to enter the value of B
+  std::cout << "Enter the value of B ";
+  std::cin >> valueB;
+
+  // Prompt the user to enter the value of C
+  std::cout << "Enter the value of C ";
+  std::cin >> valueC;
+
+  // calculate the equation
+  delta = (valueB * valueB) - 4 * (valueA * valueC);
+
+  // Set the precision to control the number of decimal numbers
+  std::cout << std::fixed << std::setprecision(1);
+
+  // Display the message
+  std::cout << "The value of delta is " << delta << std::endl;
+
+  // This ensures that the user sees the message
+  std::cout << "Press Enter to exit...";
+  std::cin.get();
+
+  return 0;
+}
+
