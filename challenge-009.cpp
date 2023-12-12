b20b774ab868aa273896995f20a5cdc6fdc04012
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-11 23:38:59 -0300

    Update challenge-009.cpp

--- challenge-009.cpp
+++ challenge-009.cpp
@@ -2,3 +2,31 @@
  9) Create an algorithm that reads how much money a person has in their wallet
  (in R$) and shows how many dollars they can buy. Consider US$1.00 = R$3.45.
 */
+
+#include <iostream>
+#include <iomanip>
+
+int main () {
+
+  // Variables
+  float avaiableBRL, valueUSD;
+
+  // Prompt the user to enter BRL value
+  std::cout << "Enter a value in BRL: ";
+  std::cin >> avaiableBRL;
+
+  // Converting BRL to USD
+  valueUSD = avaiableBRL / 3.45;
+
+  // Set the precision to control the number of decimal numbers
+  std::cout << std::fixed << std::setprecision(2);
+
+  // Display the message
+  std::cout << "You can buy a total of " << valueUSD << " USD" << std::endl;
+  
+  // This ensures that the user sees the message
+  std::cout << "Press Enter to exit...";
+  std::cin.get();
+
+  return 0;
+}
