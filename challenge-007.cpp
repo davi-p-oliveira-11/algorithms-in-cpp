a439fb9827baa53dea3698668215143e7214d50c
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-11 23:04:03 -0300

    Create challenge-007.cpp

---
+++ challenge-007.cpp
@@ -0,0 +1,7 @@
+/*
+ 7) Create an algorithm that reads a real number and displays on the screen its double and one-third.
+    Example:
+    Enter a number: 3.5
+    The double of 3.5 is 7.0
+    One-third of 3.5 is 1.16666
+*/
