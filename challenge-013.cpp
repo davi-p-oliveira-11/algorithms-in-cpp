92ee0ba1b15608a34a097179cd68dcb6a0ff4fd9
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-15 19:28:51 -0300

    Update challenge-013.cpp

--- challenge-013.cpp
+++ challenge-013.cpp
@@ -4,3 +4,35 @@
 */
 
 /* */
+
+#include <iostream>
+#include <iomanip>
+
+int main () {
+  
+  // Variables
+  float salary, raise, newSalary;
+
+  // Prompt the user to enter their salary
+  std::cout << "Enter the value of your salary: ";
+  std::cin >> salary;
+
+  // Calculate the raise 
+  raise = salary * 0.15;
+
+  // Calculate new salary
+  newSalary = salary + raise;
+
+  // Set the precision to control the number of decimal numbers
+  std::cout << std::fixed << std::setprecision(2);
+
+  // Display the message
+  std::cout << "The value of the new salary is " << newSalary << std::endl;
+
+  // This ensures that the user sees the message
+  std::cout << "Press Enter to exit...";
+  std::cin.get();
+
+
+  return 0;
+}
