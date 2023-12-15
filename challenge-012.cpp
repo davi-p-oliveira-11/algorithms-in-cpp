95159f353353e868c0fe544d069d4c720ea12ccf
Davi P. Oliveira <dpo.dev11@gmail.com>
2023-12-15 19:27:26 -0300

    Update challenge-012.cpp

--- challenge-012.cpp
+++ challenge-012.cpp
@@ -4,3 +4,31 @@ calculates, and displays its PROMOTIONAL PRICE with a 5% discount.
 */
 
 /**/
+
+#include <iostream>
+
+int main () {
+
+  // Variables
+  float productPrice, discount, finalPrice;
+
+  // Prompt the user to enter the price of the product
+  std::cout << "Enter the price of the product: ";
+  std::cin >> productPrice;
+
+  // Calculate the discount
+  discount = productPrice * 0.05;
+
+  // Calculate the final price with the discount applied
+  finalPrice = productPrice - discount;
+
+  // Show the message
+  std::cout << "The original price of the product is " << productPrice << std::endl;
+  std::cout << "The price with discount is " << finalPrice << std::endl;
+
+  // This ensures that the user sees the message
+  std::cout << "Press Enter to exit...";
+  std::cin.get();
+
+  return 0;
+}
